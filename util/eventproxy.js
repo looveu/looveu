4ac37941a97fcce071a55a5fb0531b8ae5829420
qiang <mickey.lqb@gmail.com>
2013-11-25 18:02:23 +0800

    c update

--- util/eventproxy.js
+++ util/eventproxy.js
@@ -16,11 +16,11 @@ define(function (require, exports, module) {
      * ```
      */
     var EventProxy = function () {
-      if (!(this instanceof EventProxy)) {
-        return new EventProxy();
-      }
-      this._callbacks = {};
-      this._fired = {};
+        if (!(this instanceof EventProxy)) {
+            return new EventProxy();
+        }
+        this._callbacks = {};
+        this._fired = {};
     };
 
     /**
@@ -30,10 +30,10 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback.
      */
     EventProxy.prototype.addListener = function (ev, callback) {
-      this._callbacks = this._callbacks || {};
-      this._callbacks[ev] = this._callbacks[ev] || [];
-      this._callbacks[ev].push(callback);
-      return this;
+        this._callbacks = this._callbacks || {};
+        this._callbacks[ev] = this._callbacks[ev] || [];
+        this._callbacks[ev].push(callback);
+        return this;
     };
     /**
      * `addListener` alias, bind
@@ -56,27 +56,28 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback.
      */
     EventProxy.prototype.removeListener = function (ev, callback) {
-      var calls = this._callbacks, i, l;
-      if (!ev) {
-        this._callbacks = {};
-      } else if (calls) {
-        if (!callback) {
-          calls[ev] = [];
-        } else {
-          var list = calls[ev];
-          if (!list) {
-            return this;
-          }
-          l = list.length;
-          for (i = 0; i < l; i++) {
-            if (callback === list[i]) {
-              list[i] = null;
-              break;
+        var calls = this._callbacks,
+            i, l;
+        if (!ev) {
+            this._callbacks = {};
+        } else if (calls) {
+            if (!callback) {
+                calls[ev] = [];
+            } else {
+                var list = calls[ev];
+                if (!list) {
+                    return this;
+                }
+                l = list.length;
+                for (i = 0; i < l; i++) {
+                    if (callback === list[i]) {
+                        list[i] = null;
+                        break;
+                    }
+                }
             }
-          }
         }
-      }
-      return this;
+        return this;
     };
     /**
      * `removeListener` alias, unbind
@@ -89,7 +90,7 @@ define(function (require, exports, module) {
      * @param {String} event Event name.
      */
     EventProxy.prototype.removeAllListeners = function (event) {
-      return this.unbind(event);
+        return this.unbind(event);
     };
 
     /**
@@ -100,26 +101,26 @@ define(function (require, exports, module) {
      * @param {Mix} data Pass in data
      */
     EventProxy.prototype.trigger = function (eventName, data) {
-      var list, ev, callback, args, i, l;
-      var both = 2;
-      var calls = this._callbacks;
-      while (both--) {
-        ev = both ? eventName : 'all';
-        list = calls[ev];
-        if (list) {
-          for (i = 0, l = list.length; i < l; i++) {
-            if (!(callback = list[i])) {
-              list.splice(i, 1);
-              i--;
-              l--;
-            } else {
-              args = both ? Array.prototype.slice.call(arguments, 1) : arguments;
-              callback.apply(this, args);
+        var list, ev, callback, args, i, l;
+        var both = 2;
+        var calls = this._callbacks;
+        while (both--) {
+            ev = both ? eventName : 'all';
+            list = calls[ev];
+            if (list) {
+                for (i = 0, l = list.length; i < l; i++) {
+                    if (!(callback = list[i])) {
+                        list.splice(i, 1);
+                        i--;
+                        l--;
+                    } else {
+                        args = both ? Array.prototype.slice.call(arguments, 1) : arguments;
+                        callback.apply(this, args);
+                    }
+                }
             }
-          }
         }
-      }
-      return this;
+        return this;
     };
     /**
      * `trigger` alias
@@ -131,19 +132,19 @@ define(function (require, exports, module) {
     EventProxy.prototype.fire = EventProxy.prototype.trigger;
 
     var later = typeof process !== 'undefined' && process.nextTick || function (fn) {
-      setTimeout(fn, 0);
-    };
+            setTimeout(fn, 0);
+        };
 
     /**
      * emitLater
      * make emit async
      */
     EventProxy.prototype.emitLater = function () {
-      var self = this;
-      var args = arguments;
-      later(function () {
-        self.trigger.apply(self, args);
-      });
+        var self = this;
+        var args = arguments;
+        later(function () {
+            self.trigger.apply(self, args);
+        });
     };
 
     /**
@@ -152,13 +153,13 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback
      */
     EventProxy.prototype.once = function (ev, callback) {
-      var self = this;
-      var wrapper = function () {
-        callback.apply(self, arguments);
-        self.unbind(ev, wrapper);
-      };
-      this.bind(ev, wrapper);
-      return this;
+        var self = this;
+        var wrapper = function () {
+            callback.apply(self, arguments);
+            self.unbind(ev, wrapper);
+        };
+        this.bind(ev, wrapper);
+        return this;
     };
 
     /**
@@ -168,9 +169,9 @@ define(function (require, exports, module) {
      * @param {Mix} data The data that will be passed to calback as arguments.
      */
     EventProxy.prototype.immediate = function (ev, callback, data) {
-      this.bind(ev, callback);
-      this.trigger(ev, data);
-      return this;
+        this.bind(ev, callback);
+        this.trigger(ev, data);
+        return this;
     };
     /**
      * `immediate` alias
@@ -178,57 +179,60 @@ define(function (require, exports, module) {
     EventProxy.prototype.asap = EventProxy.prototype.immediate;
 
     var _assign = function (eventname1, eventname2, cb, once) {
-      var proxy = this, length, index = 0, argsLength = arguments.length,
-        bind, _all,
-        callback, events, isOnce, times = 0, flag = {};
+        var proxy = this,
+            length, index = 0,
+            argsLength = arguments.length,
+            bind, _all,
+            callback, events, isOnce, times = 0,
+            flag = {};
 
-      // Check the arguments length.
-      if (argsLength < 3) {
-        return this;
-      }
-
-      events = Array.prototype.slice.apply(arguments, [0, argsLength - 2]);
-      callback = arguments[argsLength - 2];
-      isOnce = arguments[argsLength - 1];
-
-      // Check the callback type.
-      if (typeof callback !== "function") {
-        return this;
-      }
-      length = events.length;
-      bind = function (key) {
-        var method = isOnce ? "once" : "bind";
-        proxy[method](key, function (data) {
-          proxy._fired[key] = proxy._fired[key] || {};
-          proxy._fired[key].data = data;
-          if (!flag[key]) {
-            flag[key] = true;
-            times++;
-          }
-        });
-      };
-
-      for (index = 0; index < length; index++) {
-        bind(events[index]);
-      }
-
-      _all = function (event) {
-        if (times < length) {
-          return;
+        // Check the arguments length.
+        if (argsLength < 3) {
+            return this;
         }
-        if (!flag[event]) {
-          return;
+
+        events = Array.prototype.slice.apply(arguments, [0, argsLength - 2]);
+        callback = arguments[argsLength - 2];
+        isOnce = arguments[argsLength - 1];
+
+        // Check the callback type.
+        if (typeof callback !== "function") {
+            return this;
         }
-        var data = [];
+        length = events.length;
+        bind = function (key) {
+            var method = isOnce ? "once" : "bind";
+            proxy[method](key, function (data) {
+                proxy._fired[key] = proxy._fired[key] || {};
+                proxy._fired[key].data = data;
+                if (!flag[key]) {
+                    flag[key] = true;
+                    times++;
+                }
+            });
+        };
+
         for (index = 0; index < length; index++) {
-          data.push(proxy._fired[events[index]].data);
+            bind(events[index]);
         }
-        if (isOnce) {
-          proxy.unbind("all", _all);
-        }
-        callback.apply(null, data);
-      };
-      proxy.bind("all", _all);
+
+        _all = function (event) {
+            if (times < length) {
+                return;
+            }
+            if (!flag[event]) {
+                return;
+            }
+            var data = [];
+            for (index = 0; index < length; index++) {
+                data.push(proxy._fired[events[index]].data);
+            }
+            if (isOnce) {
+                proxy.unbind("all", _all);
+            }
+            callback.apply(null, data);
+        };
+        proxy.bind("all", _all);
     };
 
     /**
@@ -244,10 +248,10 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback, that will be called after predefined events were fired.
      */
     EventProxy.prototype.all = function (eventname1, eventname2, callback) {
-      var args = Array.prototype.concat.apply([], arguments);
-      args.push(true);
-      _assign.apply(this, args);
-      return this;
+        var args = Array.prototype.concat.apply([], arguments);
+        args.push(true);
+        _assign.apply(this, args);
+        return this;
     };
     /**
      * `all` alias
@@ -259,12 +263,12 @@ define(function (require, exports, module) {
      * @param {Function(err)} callback
      */
     EventProxy.prototype.fail = function (callback) {
-      var that = this;
-      that.once('error', function (err) {
-        that.unbind();
-        callback(err);
-      });
-      return this;
+        var that = this;
+        that.once('error', function (err) {
+            that.unbind();
+            callback(err);
+        });
+        return this;
     };
 
     /**
@@ -281,10 +285,10 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback, that will be called after predefined events were fired.
      */
     EventProxy.prototype.tail = function () {
-      var args = Array.prototype.concat.apply([], arguments);
-      args.push(false);
-      _assign.apply(this, args);
-      return this;
+        var args = Array.prototype.concat.apply([], arguments);
+        args.push(false);
+        _assign.apply(this, args);
+        return this;
     };
     /**
      * `tail` alias, assignAll
@@ -302,39 +306,39 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback, that will be called after event was fired N times.
      */
     EventProxy.prototype.after = function (eventName, times, callback) {
-      if (times === 0) {
-        callback.call(null, []);
+        if (times === 0) {
+            callback.call(null, []);
+            return this;
+        }
+        var proxy = this,
+            firedData = [],
+            all;
+        this._after = this._after || {};
+        var group = eventName + '_group';
+        this._after[group] = {
+            index: 0,
+            results: []
+        };
+        all = function (name, data) {
+            if (name === eventName) {
+                times--;
+                firedData.push(data);
+                if (times < 1) {
+                    proxy.unbind("all", all);
+                    callback.apply(null, [firedData]);
+                }
+            }
+            if (name === group) {
+                times--;
+                proxy._after[group].results[data.index] = data.result;
+                if (times < 1) {
+                    proxy.unbind("all", all);
+                    callback.call(null, proxy._after[group].results);
+                }
+            }
+        };
+        proxy.bind("all", all);
         return this;
-      }
-      var proxy = this,
-        firedData = [],
-        all;
-      this._after = this._after || {};
-      var group = eventName + '_group';
-      this._after[group] = {
-        index: 0,
-        results: []
-      };
-      all = function (name, data) {
-        if (name === eventName) {
-          times--;
-          firedData.push(data);
-          if (times < 1) {
-            proxy.unbind("all", all);
-            callback.apply(null, [firedData]);
-          }
-        }
-        if (name === group) {
-          times--;
-          proxy._after[group].results[data.index] = data.result;
-          if (times < 1) {
-            proxy.unbind("all", all);
-            callback.call(null, proxy._after[group].results);
-          }
-        }
-      };
-      proxy.bind("all", all);
-      return this;
     };
 
     /**
@@ -354,20 +358,20 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback function, should return the final result.
      */
     EventProxy.prototype.group = function (eventName, callback) {
-      var that = this;
-      var group = eventName + '_group';
-      var index = that._after[group].index;
-      that._after[group].index++;
-      return function (err, data) {
-        if (err) {
-          return that.emit('error', err);
-        }
-        that.emit(group, {
-          index: index,
-          // callback(err, args1, args2, ...)
-          result: callback ? callback.apply(null, Array.prototype.slice.call(arguments, 1)) : data
-        });
-      };
+        var that = this;
+        var group = eventName + '_group';
+        var index = that._after[group].index;
+        that._after[group].index++;
+        return function (err, data) {
+            if (err) {
+                return that.emit('error', err);
+            }
+            that.emit(group, {
+                index: index,
+                // callback(err, args1, args2, ...)
+                result: callback ? callback.apply(null, Array.prototype.slice.call(arguments, 1)) : data
+            });
+        };
     };
 
     /**
@@ -377,26 +381,29 @@ define(function (require, exports, module) {
      * @param {Function} callback The callback will get a map that has data and eventName attributes.
      */
     EventProxy.prototype.any = function () {
-      var proxy = this,
-        index,
-        _bind,
-        len = arguments.length,
-        callback = arguments[len - 1],
-        events = Array.prototype.slice.apply(arguments, [0, len - 1]),
-        count = events.length,
-        _eventName = events.join("_");
+        var proxy = this,
+            index,
+            _bind,
+            len = arguments.length,
+            callback = arguments[len - 1],
+            events = Array.prototype.slice.apply(arguments, [0, len - 1]),
+            count = events.length,
+            _eventName = events.join("_");
 
-      proxy.once(_eventName, callback);
+        proxy.once(_eventName, callback);
 
-      _bind = function (key) {
-        proxy.bind(key, function (data) {
-          proxy.trigger(_eventName, {"data": data, eventName: key});
-        });
-      };
+        _bind = function (key) {
+            proxy.bind(key, function (data) {
+                proxy.trigger(_eventName, {
+                    "data": data,
+                    eventName: key
+                });
+            });
+        };
 
-      for (index = 0; index < count; index++) {
-        _bind(events[index]);
-      }
+        for (index = 0; index < count; index++) {
+            _bind(events[index]);
+        }
     };
 
     /**
@@ -405,12 +412,12 @@ define(function (require, exports, module) {
      * @param {Function} callback Callback.
      */
     EventProxy.prototype.not = function (eventName, callback) {
-      var proxy = this;
-      proxy.bind("all", function (name, data) {
-        if (name !== eventName) {
-          callback(data);
-        }
-      });
+        var proxy = this;
+        proxy.bind("all", function (name, data) {
+            if (name !== eventName) {
+                callback(data);
+            }
+        });
     };
 
     /**
@@ -447,45 +454,45 @@ define(function (require, exports, module) {
      * @return {Function}
      */
     EventProxy.prototype.done = function (handler, callback) {
-      var that = this;
-      return function (err, data) {
-        if (err) {
-          return that.emit('error', err);
-        }
+        var that = this;
+        return function (err, data) {
+            if (err) {
+                return that.emit('error', err);
+            }
 
-        // callback(err, args1, args2, ...)
-        var args = Array.prototype.slice.call(arguments, 1);
+            // callback(err, args1, args2, ...)
+            var args = Array.prototype.slice.call(arguments, 1);
 
-        if (typeof handler === 'string') {
-          // getAsync(query, ep.done('query'));
-          // or
-          // getAsync(query, ep.done('query', function (data) {
-          //   return data.trim();
-          // }));
-          return that.emit(handler,  callback ? callback.apply(null, args) : data);
-        }
+            if (typeof handler === 'string') {
+                // getAsync(query, ep.done('query'));
+                // or
+                // getAsync(query, ep.done('query', function (data) {
+                //   return data.trim();
+                // }));
+                return that.emit(handler, callback ? callback.apply(null, args) : data);
+            }
 
-        // speed improve for mostly case: `callback(err, data)`
-        if (arguments.length <= 2) {
-          return handler(data);
-        }
+            // speed improve for mostly case: `callback(err, data)`
+            if (arguments.length <= 2) {
+                return handler(data);
+            }
 
-        // callback(err, args1, args2, ...)
-        handler.apply(null, args);
-      };
+            // callback(err, args1, args2, ...)
+            handler.apply(null, args);
+        };
     };
 
     /**
      * make done async
      */
     EventProxy.prototype.doneLater = function (handler, callback) {
-      var _doneHandler = this.done(handler, callback);
-      return function (err, data) {
-        var args = arguments;
-        later(function () {
-          _doneHandler.apply(null, args);
-        });
-      };
+        var _doneHandler = this.done(handler, callback);
+        return function (err, data) {
+            var args = arguments;
+            later(function () {
+                _doneHandler.apply(null, args);
+            });
+        };
     };
 
     /**
@@ -504,18 +511,18 @@ define(function (require, exports, module) {
      * @returns {EventProxy} EventProxy instance
      */
     EventProxy.create = function () {
-      var ep = new EventProxy();
-      var args = Array.prototype.concat.apply([], arguments);
-      if (args.length) {
-        var errorHandler = args[args.length - 1];
-        var callback = args[args.length - 2];
-        if (typeof errorHandler === 'function' && typeof callback === 'function') {
-          args.pop();
-          ep.fail(errorHandler);
+        var ep = new EventProxy();
+        var args = Array.prototype.concat.apply([], arguments);
+        if (args.length) {
+            var errorHandler = args[args.length - 1];
+            var callback = args[args.length - 2];
+            if (typeof errorHandler === 'function' && typeof callback === 'function') {
+                args.pop();
+                ep.fail(errorHandler);
+            }
+            ep.assign.apply(ep, Array.prototype.slice.call(args));
         }
-        ep.assign.apply(ep, Array.prototype.slice.call(args));
-      }
-      return ep;
+        return ep;
     };
 
     // Backwards compatibility
